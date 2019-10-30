ab09ac624ce913789f7ab5e1e904d52b3168b4c3
melissam <melissam@ciandt.com>
2019-10-30 14:15:25 -0700

    Updates

---
+++ precache-manifest.0a54babaeac7664a1572459e660507b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c1af7de7ccc36e935b13f61457d630",
+    "url": "/card-demo/index.html"
+  },
+  {
+    "revision": "f043920c02be5dbb7626",
+    "url": "/card-demo/static/css/main.1324aa1f.chunk.css"
+  },
+  {
+    "revision": "3c1fa33a72778b87f042",
+    "url": "/card-demo/static/js/2.8d6d6382.chunk.js"
+  },
+  {
+    "revision": "f043920c02be5dbb7626",
+    "url": "/card-demo/static/js/main.1087c750.chunk.js"
+  },
+  {
+    "revision": "a0c97efadd7be545f09b",
+    "url": "/card-demo/static/js/runtime-main.75d415e6.js"
+  }
+]);
